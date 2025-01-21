d952e9b4a25598b86fde4be70b47147e54ce0945
monelcocou <monelcocou@gmail.com>
2025-01-21 21:16:59 +0100

    feat: implementation des cardviews

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
     return (
-        <div className=''>
-            AdminPage
+        <div className='p-4 flex gap-4 flex-col md:flex-row'>
+            <div className="w-full lg:w-2/3">
+                <div className="flex gap-4 justify-between flex-wrap">
+                    <UserCard type="students"/>
+                    <UserCard type="teachers"/>
+                    <UserCard type="parents"/>
+                    <UserCard type="staffs"/>
+                </div>
+            </div>
+
+
+            <div className="w-full lg:w-1/3">r</div>
         </div>
     );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                lamaSky: "#C3EBFA",
+                lamaSkyLight: "#EDF9FD",
+                lamaPurple: "#CFCEFF",
+                lamaPurpleLight: "#F1F0FF",
+                lamaYellow: "#FAE27C",
+                lamaYellowLight: "#FEFCE8",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
