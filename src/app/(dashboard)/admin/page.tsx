77737ae51a60c8280cbf265ecdb6a3ad3679917a
monelcocou <monelcocou@gmail.com>
2025-01-21 22:06:56 +0100

    feat: implementation de la chart countStudents

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
 
 const AdminPage = () => {
     return (
         <div className='p-4 flex gap-4 flex-col md:flex-row'>
-            <div className="w-full lg:w-2/3">
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
                 <div className="flex gap-4 justify-between flex-wrap">
                     <UserCard type="students"/>
                     <UserCard type="teachers"/>
                     <UserCard type="parents"/>
                     <UserCard type="staffs"/>
+                </div>
+
+
+                <div className="flex gap-4 flex-col lg:flex-row">
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart/>
+                    </div>
+
+                    <div className="w-full lg:w-2/3 h-[450px]">
+
+                    </div>
+                </div>
+
+
+                <div>
+                    Bottom
                 </div>
             </div>
 
